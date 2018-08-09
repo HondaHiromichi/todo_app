b9520c968204212f6361d53142389b7f9bbc85a1
Honda Hiromichi <>
2018-08-09 21:11:15 +0900

    Deviseの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def flash_message(message, klass)
+        content_tag(:div, class: "alert alert-#{klass}") do
+            concat content_tag(:button, 'x', class: 'close', date: {dismiss: 'alart'})
+            concat raw(message)
+        end
+    end
 end
+
