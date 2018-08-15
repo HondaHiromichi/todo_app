3c51762d2e6ab5c4cf66ce72db17bd711ed126f3
Honda Hiromichi <>
2018-08-15 21:34:38 +0900

    管理画面の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  rescue_from SecurityError do |exception|
+    redirect_to root_url, notice: 'アドミン画面へのアクセス権限がありません。'
+  end
+
+  protected
+
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.try(:admin?)
+  end 
 end
