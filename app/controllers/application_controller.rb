f0c9bcae49e07632b6af53239be00d9f21f5beed
Honda Hiromichi <>
2018-08-21 20:32:03 +0900

    usernameの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
   rescue_from SecurityError do |exception|
     redirect_to root_url, notice: 'アドミン画面へのアクセス権限がありません。'
+　protect_from_forgery with: :exception
   end
 
   protected
@@ -12,7 +13,7 @@ class ApplicationController < ActionController::Base
     raise SecurityError unless current_user.try(:admin?)
   end 
   def configure_permitted_parameters
-    added_attrs = [ :username, :email, :password, :password_confirmation　]
+    added_attrs = [ :name, :email, :password, :password_confirmation　]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
