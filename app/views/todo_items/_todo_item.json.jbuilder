1483e535556bef2dc894ee104929f87ba66ae4e0
Honda Hiromichi <>
2018-08-25 19:20:45 +0900

    scaffoldで作られたファイルを削除

--- app/views/todo_items/_todo_item.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! todo_item, :id, :title, :contents, :created_at, :updated_at
-json.url todo_item_url(todo_item, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :todo_items
   ActiveAdmin.routes(self)
   root 'home#index'
   devise_for :users
