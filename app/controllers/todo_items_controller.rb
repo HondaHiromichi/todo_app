d2ea11f0f1c381a4c2d8d042b7538917e37949a9
Honda Hiromichi <>
2018-08-29 21:23:53 +0900

    updateまで実装

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,4 +2,52 @@ class TodoItemsController < ApplicationController
   def index
     @todo_items = TodoItem.all
   end
+
+  def show
+    # binding.pry 
+    @todo_item = TodoItem.find(params[:id])
+  end
+
+  def new
+    @todo_item = TodoItem.new
+  end
+
+  def edit
+    @todo_item = TodoItem.find(params[:id])
+  end
+
+  def create
+    @todo_item = TodoItem.new(todo_item_params)
+    if @todo_item.save
+      redirect_to @todo_item, notice: "todoを登録しました"
+    else
+      render :new
+    end
+  end
+
+  def update
+    # binding.pry
+    @todo_item = TodoItem.find(params[:id])
+    render plain: @todo_item.title
+    return
+    if @todo_item.update(todo_item_params)
+      redirect_to @todo_item, notice: "todoを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo_item.destroy
+    respond_to do |format|
+    redirect_to todo_item, notice: "todoを削除しました"
+    end
+  end
+  
+
+  private
+
+  def todo_item_params
+      params.require(:todo_item).permit(:title, :contents)
+  end
 end
